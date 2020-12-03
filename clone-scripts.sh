cf789fa5af596d83ff564d12dbd2bb0ae5f187b1
Bakhtiar Durrani <69326578+bakhtiardurrani@users.noreply.github.com>
2020-12-03 11:28:26 +0500

    Update clone-scripts.sh

--- clone-scripts.sh
+++ clone-scripts.sh
@@ -10,17 +10,26 @@ EOF
   fi
 }
 
+function check-ssh() {
+    if [[ ! -f /home/$(whoami)/.ssh/id_rsa ]]; then
+        echo "Kindly upload your private key and restart the process."
+        exit 1
+    else
+    eval $(ssh-agent -s)
+    ssh-add /home/$(whoami)/.ssh/id_rsa
+    fi
 function clone() {
     sudo rm -rf ~/scripts
     git clone --quiet git@github.com:bakhtiardurrani/scripts.git ~/scripts
     sudo chown -cR 1000:1000 ~/scripts/ 1>/dev/null 2>&1
     sudo chmod -cR 755 ~/scripts/ >> /dev/null 1>/dev/null 2>&1
     if [[ ! -d "/home/$(whoami)/scripts" ]]; then
-        echo "scripts directory not available you don't have github ssh access!"
+        echo "scripts directory not available you don't have github ssh access."
         exit 1
     else
     sudo bash ~/scripts/after-install.sh
     fi
 }
 # Sudo check depricated due to ssh access
+check-ssh
 clone
